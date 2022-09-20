81f5992cee3bbcdf6fd67c40309e95f8176bb2b9
abdel-riahi <113486450+abdel-riahi@users.noreply.github.com>
2022-09-21 00:53:06 +0200

    add new parameters  to controller function

--- src/Controller/ApiMoviesController.php
+++ src/Controller/ApiMoviesController.php
@@ -14,47 +14,42 @@ class ApiMoviesController extends AbstractController
     private $apiService;
     private $apiGenreService;
     public function __construct(ApiCallService $apiService, ApiGenreService $apiGenreService)
-{
-  $this->apiService = $apiService ;
-  $this->apiGenreService = $apiGenreService ;
-}
-
+    {
+    $this->apiService = $apiService ;
+    $this->apiGenreService = $apiGenreService ;
+    }
     #[Route('/api/post/movies', name: 'api_movies')]
     public function apiMovies(Request $request): Response
     {
         $requestQuery = $request->query->get('query');
         $param = !empty($requestQuery) ? 'search' : null;
         $query = $requestQuery ?? null;
-        $result =  $this->willCall($query, $param); 
+        $result =  $this->willCall($query, $param);
         return $this->render('api_movies/movies.html.twig', [
-        'genre' =>$result["genre"]["genres"], 'movies' =>$result["movies"]["results"],
+        'genre' =>$result["genre"]["genres"], 
+        'movies' =>$result["movies"]["results"],
         'key' => $result["movies"]["key"]
         ]);
     }
-
-    #[Route('/api/post/movie/{id}', name: 'veiw_movie', requirements: ['id' => '\d+'])]
+    #[Route('/api/post/movie/{id}', name: 'view_movie')] 
     public function apiMovieDetails(int $id): Response
     {
         $param = 'videos';
         $result =  $this->willCall($id, $param);
-        
         return $this->render('api_movies/view_movie.html.twig', [
-            "movie" =>$result["movies"]["results"][0],
-            
+        "movie" =>$result["movies"]["results"][0],   
         ]);
     }
-
     #[Route('/api/post/movies_genre/{id}', name: 'view_movie_genre')]
     public function apiMoviesSerchyGenre(int $id): Response
     {
         $param = "discover";
         $result =  $this->willCall($id, $param);
         return $this->render('api_movies/movies_genre.html.twig', [
-            "movies" =>$result["movies"]["results"]
+        "movies" =>$result["movies"]["results"]
         ]);
     }
-
-    private function willCall($query, $param)
+    private function willCall($query, $param) : array
     {
        $paramsQuery = ["query" => $query, "param" => $param];
        $movies = $this->apiService->callApi($paramsQuery);
@@ -63,6 +58,5 @@ class ApiMoviesController extends AbstractController
     }
    
 }
-
 
 
